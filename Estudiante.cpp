eda5fbde408bead1dd75b0483601625aa53b1810
wilsonamaya <142435055+wilsonamaya@users.noreply.github.com>
2024-03-04 13:38:59 -0400

    04/03/2024

---
+++ Estudiante.cpp
@@ -0,0 +1,27 @@
+#include "Estudiante.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Estudiante::Estudiante()
+{
+
+}
+Estudiante::~Estudiante()
+{
+
+}
+
+void Estudiante::set_nombre(string _n)
+{
+	//
+	//a valores al atributo nombre de la clase (nombre)
+	nombre = _n;
+}
+
+string Estudiante::get_nombre()
+{
+	return string();
+}
+
